be68fece0c6322b8563ee8a8fdee5f45e035d654
Daniella Stensland <dellasten25@gmail.com>
2026-02-12 11:30:07 +0100

    task 3

--- src/main/java/no/hvl/dat110/system/display/DisplayImpl.java
+++ src/main/java/no/hvl/dat110/system/display/DisplayImpl.java
@@ -1,9 +1,8 @@
 package no.hvl.dat110.system.display;
 
-import no.hvl.dat110.TODO;
 import no.hvl.dat110.rpc.RPCRemoteImpl;
-import no.hvl.dat110.rpc.RPCUtils;
 import no.hvl.dat110.rpc.RPCServer;
+import no.hvl.dat110.rpc.RPCUtils;
 
 public class DisplayImpl extends RPCRemoteImpl {
 
@@ -22,11 +21,11 @@ public class DisplayImpl extends RPCRemoteImpl {
 		// TODO - START: 
 		// implement unmarshalling, call, and marshall for write RPC method
 		// look at how this is done in the SensorImpl class for the read method
-		
-		if (true)
-			throw new UnsupportedOperationException(TODO.method());
-		
-		// TODO - END
+		String message = RPCUtils.unmarshallString(param);
+
+		write(message);
+
+		returnval = RPCUtils.marshallVoid();
 		
 		return returnval;
 	}
